44fd2c22d57147067fe2fa95b281077760aadee3
Yoojin Ko <db232100@gmail.com>
2024-04-24 17:43:14 +0900

    feat: add buttons and status for test flows

--- src/containers/arenaChat.tsx
+++ src/containers/arenaChat.tsx
@@ -2,11 +2,33 @@
 
 import ChatBox from "@/components/chatBox";
 import PromptInput from "@/components/promptInput";
-import { Flex } from "antd";
+import { Button, Flex } from "antd";
 import { useState } from "react";
 
-export default function ArenaChat() {
+type ArenaChatProps = {
+  modelA: string,
+  modelB: string,
+}
+
+enum ArenaStatus {
+  READY = 'READY',
+  COMPETING = 'COMPETING',
+  END = 'END',
+}
+
+export default function ArenaChat({modelA, modelB}: ArenaChatProps) {
   const [prompt, setPrompt] = useState('');
+  const [status, setStatus] = useState<ArenaStatus>(ArenaStatus.READY)
+
+  const onClickReadyBtn = (e: any) => {
+    console.log('e.target.value :>> ', e.target.value);
+    setStatus(ArenaStatus.END);
+  }
+
+  const handlePrompt = (prompt: string) => {
+    setPrompt(prompt);
+    setStatus(ArenaStatus.COMPETING);
+  }
 
   return (
     <div>
@@ -14,7 +36,13 @@ export default function ArenaChat() {
         <ChatBox modelName="Model A" prompt={prompt} />
         <ChatBox modelName="Model B" prompt={prompt} />
       </Flex>
-      <PromptInput setParentPrompt={setPrompt} />
+      <Button onClick={onClickReadyBtn} value={'Model A'} disabled={status !== ArenaStatus.COMPETING}>Model A</Button>
+      <Button onClick={onClickReadyBtn} value={'Model B'} disabled={status !== ArenaStatus.COMPETING}>Model B</Button>
+      <Button onClick={onClickReadyBtn} value={'Tie'} disabled={status !== ArenaStatus.COMPETING}>Tie</Button>
+      <Button onClick={onClickReadyBtn} value={'Nothing'} disabled={status !== ArenaStatus.COMPETING}>Nothing</Button>
+      <PromptInput setParentPrompt={handlePrompt} />
+      {status === ArenaStatus.END ? (<Button>NextChalenge</Button>) : (<></>) }
+      
     </div>
   )
 }
