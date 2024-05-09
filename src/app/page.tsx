371e1d14c4f9464fb1b4904b26fb518e52bfafef
Yoojin Ko <db232100@gmail.com>
2024-05-09 15:08:26 +0900

    feat: temp add dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Dashboard from "@/containers/dashboard";
 import ServerArena from "@/containers/serverArena";
 
 export default function Home() {
   return (
     <main className="min-h-screen justify-between p-24">
       <ServerArena />
+      <Dashboard /> {/* FIXME(yoojin): add choice components */}
     </main>
   );
 }
