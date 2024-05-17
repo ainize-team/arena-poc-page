f1380fde3fc764ce96a99fdcdbc07d811357e4af
Yoojin Ko <db232100@gmail.com>
2024-05-17 09:32:49 +0900

    fix: chat result size with vh

--- src/components/chatBox.tsx
+++ src/components/chatBox.tsx
@@ -17,7 +17,7 @@ export default function ChatBox({ modelName, status, prompt}: ChatBoxProps) {
   function textboxContent(status:ArenaStatus, prompt?: string) {
     if (prompt) {
       return <Paragraph style={{
-        height: 500 - 60, // FIXME(yoojin): change height to not constant value.
+        height: `${60 - 3}vh`, // FIXME(yoojin): change height to not constant value.
         overflow: "auto",
       }}>{
         splitPrompt(prompt).map((line: string, index: number) => {
