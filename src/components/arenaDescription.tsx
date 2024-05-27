bbf2446e61e31b6133cc8b7b698ef12dfddeeaca
Yoojin Ko <db232100@gmail.com>
2024-05-27 17:51:48 +0900

    fix: description padding

--- src/components/arenaDescription.tsx
+++ src/components/arenaDescription.tsx
@@ -2,11 +2,11 @@ import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 
 export default function ArenaDescription() {
-  const prompt = `### 📜 Rules
+  const rules = `### 📜 Rules
   1. Connect your [AIN Wallet](https://chromewebstore.google.com/detail/ain-wallet/hbdheoebpgogdkagfojahleegjfkhkpl?hl=ko) so that you can get AIN credit for your contribution.
   2. Ask any question to two anonymous models (e.g., ChatGPT, Claude, Llama) and vote for the better one!
-  3. You will be rewarded with AIN credit based on the quality of your prompt and evaluation, so please be thoughtful and thorough in your participation.
-### 👇 Chat now!`
+  3. You will be rewarded with AIN credit based on the quality of your prompt and evaluation, so please be thoughtful and thorough in your participation.`
+  const chatNow = `### 👇 Chat now!`
   return (
     <div style={{
       background: "#ffffff",
@@ -21,8 +21,13 @@ export default function ArenaDescription() {
           <a style={{color: "blue"}} {...props} />
         )
       }}>
-        {prompt}
+        {rules}
       </ReactMarkdown>
+      <div style={{marginTop: "8px"}}>
+        <ReactMarkdown>
+          {chatNow}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 }
