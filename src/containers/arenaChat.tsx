18728349989b3bd687eb13bc8a8324fdf4fb3d73
Yoojin Ko <db232100@gmail.com>
2024-05-31 11:25:20 +0900

    feat: noti duration. never close automatically

--- src/containers/arenaChat.tsx
+++ src/containers/arenaChat.tsx
@@ -48,6 +48,7 @@ export default function ArenaChat() {
       description:
         isZeroScore ? "Prompt was not meaningful for us. Try different prompt!" : `Reward: ${rewardData.reward} AIN`,
       placement: "topRight",
+      duration: 0,
     });
   };
 
