9073b1438211730b1da782224e9f13ae45e4a7a4
Yoojin Ko <db232100@gmail.com>
2024-05-31 10:08:00 +0900

    feat: add winner mark

--- src/containers/arenaChat.tsx
+++ src/containers/arenaChat.tsx
@@ -132,6 +132,7 @@ export default function ArenaChat() {
         method: "POST",
         body: JSON.stringify(chatResultParams),
       })).json();
+      changeWinnerName(value);
       setStatus(ArenaStatus.END);
       const reward = await fetch(`/api/arena/reward/${battleId}`, {
         method: "GET",
@@ -139,6 +140,24 @@ export default function ArenaChat() {
       openNotification(await reward.json());
     } catch (err) {
       alert(`${err}.\n If the error is repeated, please refresh the page.`);
+    }
+  }
+
+  const changeWinnerName = (winner: ChoiceType) => {
+    const winnerMark = "🎉";
+    switch(winner) {
+      case ChoiceType.MODELA:
+        setModelA(winnerMark + modelA);
+        break;
+      case ChoiceType.MODELB:
+        setModelB(winnerMark + modelB);
+        break;
+      case ChoiceType.TIE:
+        setModelA(winnerMark + modelA);
+        setModelB(winnerMark + modelB);
+        break;
+      case ChoiceType.NOTHING:
+        break;
     }
   }
 
