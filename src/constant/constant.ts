e7884181789b474164caeb0f18dd383eecd2b30e
Yoojin Ko <db232100@gmail.com>
2024-06-03 15:28:35 +0900

    fix: ga id

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -6,5 +6,5 @@ export const PUBLIC_ENV = {
 export const ZERO_REWARD_MSG = "NO TX FOR 0 REWARD";
 
 export const getGAId = () => {
-  return process.env.APP_ENV === "production" ? "G-TFVPRQGF0M" : "G-WHKH2Q8EJY"
+  return PUBLIC_ENV.APP_ENV === "production" ? "G-TFVPRQGF0M" : "G-WHKH2Q8EJY"
 };
