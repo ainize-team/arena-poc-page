8d4302cd40b59ce0aafc4fca8209966e1aafebce
Yoojin Ko <db232100@gmail.com>
2024-06-03 17:11:28 +0900

    debug: captcha log

--- src/app/api/arena/recaptchaSubmit/route.ts
+++ src/app/api/arena/recaptchaSubmit/route.ts
@@ -2,39 +2,38 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 
 export async function POST(request: Request, response: Response) {
-    const secretKey = process.env.CAPTCHA_SECRET_KEY;
+  const secretKey = process.env.CAPTCHA_SECRET_KEY;
+  const postData = await request.json();
+  const { gRecaptchaToken } = postData;
 
-    const postData = await request.json();
+  console.log('secretKey :>> ', secretKey);
 
-    const { gRecaptchaToken } = postData;
+  let res;
+  const formData = `secret=${secretKey}&response=${gRecaptchaToken}`;
+  try {
+    res = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    console.log('res :>> ', res);
+  } catch (e) {
+    console.log('e :>> ', e);
+    return NextResponse.json({ success: false })
+  }
 
-    let res;
+  if (res && res.data?.success && res.data?.score > 0.5) {
+    console.log("res.data?.score:", res.data?.score);
 
-    const formData = `secret=${secretKey}&response=${gRecaptchaToken}`;
-
-    try {
-        res = await axios.post(
-            "https://www.google.com/recaptcha/api/siteverify",
-            formData,
-            {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-            }
-        );
-
-    } catch (e) {
-        return NextResponse.json({ success: false })
-    }
-
-    if (res && res.data?.success && res.data?.score > 0.5) {
-        console.log("res.data?.score:", res.data?.score);
-
-        return NextResponse.json({
-            success: true,
-            score: res.data.score,
-        });
-    } else {
-        return NextResponse.json({ success: false });
-    }
+    return NextResponse.json({
+      success: true,
+      score: res.data.score,
+    });
+  } else {
+      return NextResponse.json({ success: false });
+  }
 }
