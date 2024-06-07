d6186e81c177e6f8ebd06b3b45ebf051ee09e0f3
Yoojin Ko <db232100@gmail.com>
2024-06-07 13:54:51 +0900

    feat: add m view layout

---
+++ src/app/m/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Layout, { Footer } from "antd/lib/layout/layout";
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  // position: "fixed",
+  height: "80px",
+  bottom: 0,
+  width: "100%"
+};
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: "hidden",
+  allignItems: "center",
+  width: "100%",
+  maxWidth: "100%",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chatbot Arena",
+  description: "Chatbot Arena powerd by AI Network.",
+};
+
+export default function MobileLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Layout style={layoutStyle}>
+            {children}
+          <Footer style={footerStyle}>Powered by AI Network © 2024</Footer>
+        </Layout>
+      </body>
+    </html>
+  );
+}
