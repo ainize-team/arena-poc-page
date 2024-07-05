b9076580a3dd723df867de3339a42880eaff0d11
Yoojin Ko <db232100@gmail.com>
2024-07-05 10:50:32 +0900

    fix: remove reward progressbar  and descriptions

--- src/components/arenaDescription.tsx
+++ src/components/arenaDescription.tsx
@@ -1,11 +1,9 @@
-import { Flex, Progress, ProgressProps } from "antd";
 import ReactMarkdown from "react-markdown";
-import RewardProgress from "./rewardProgress";
+
 export default function ArenaDescription() {
   const rules = `### 📜 Rules
-  1. Connect your [AIN Wallet](https://chromewebstore.google.com/detail/ain-wallet/hbdheoebpgogdkagfojahleegjfkhkpl?hl=ko) so that you can get AIN for your contribution.
-  2. Ask any question to two anonymous models (e.g., ChatGPT, Claude, Llama) and vote for the better one!
-  3. You will be rewarded with AIN based on the quality of your prompt and evaluation, so please be thoughtful and thorough in your participation.`
+  1. Connect your [AIN Wallet](https://chromewebstore.google.com/detail/ain-wallet/hbdheoebpgogdkagfojahleegjfkhkpl?hl=ko).
+  2. Ask any question to two anonymous models (e.g., ChatGPT, Claude, Llama) and vote for the better one!`
   const chatNow = `### 👇 Chat now!`
   return (
     <div style={{
@@ -16,7 +14,6 @@ export default function ArenaDescription() {
       paddingInline: "3rem",
       lineHeight: 1.5,
     }}>
-      <RewardProgress />
       <ReactMarkdown components={{
         a: (props: any) => (
           <a style={{color: "blue"}} {...props} />
--- src/containers/arenaChat.tsx
+++ src/containers/arenaChat.tsx
@@ -74,11 +74,9 @@ export default function ArenaChat() {
   };
 
   const openNotification = (rewardData: any) => {
-    const isZeroReward = rewardData.reward === 0;
     notiApi.info({
-      message: isZeroReward ? "Reward Failed." : "Reward Success!",
-      description:
-        isZeroReward ? rewardData.reason : `Reward: ${rewardData.reward} AIN`,
+      message: "Thank you for voting!",
+      description: rewardData.reason,
       placement: "topRight",
       duration: 0,
     });
