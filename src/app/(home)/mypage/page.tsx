afbdb8cd9edba07ec928f1ad0691bba0fa5927b1
Yunsu Bae <yunsu@comcom.ai>
2024-09-11 17:01:51 +0900

    remove unused line

--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -6,16 +6,13 @@ import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 import { Spin } from "antd";
 
-import useAuth from "@/src/lib/auth";
 import useInfo from "@/src/utils/handleGetInfo";
 import { LoadingOutlined } from "@ant-design/icons";
 import Mypage from "@/src/containers/mypage";
 import { userInfoState } from "@/src/lib/recoil";
-import { UserInfo } from "@/src/types/type";
 
 export default function Home() {
   const router = useRouter();
-  const { authFetch } = useAuth();
   const { getInfo } = useInfo();
 
   const [recoilUserInfoState, setUserInfo] = useRecoilState(userInfoState);
